1843a3320ebbba43da815dcdc1583f15c4739a54
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2024-06-28 20:08:47 -0300

    feat: adding EmailController in project

---
+++ src/main/java/br/com/rafael/microserviceemail/controllers/EmailController.java
@@ -0,0 +1,27 @@
+package br.com.rafael.microserviceemail.controllers;
+
+import br.com.rafael.microserviceemail.dtos.EmailDto;
+import br.com.rafael.microserviceemail.models.EmailModel;
+import br.com.rafael.microserviceemail.services.EmailService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailController {
+    @Autowired
+    EmailService emailService;
+
+    @PostMapping("/sending-email")
+    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto) {
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+    }
+}
