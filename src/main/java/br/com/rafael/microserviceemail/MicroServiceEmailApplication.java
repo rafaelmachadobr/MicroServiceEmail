aa91d2607b3a001463c0c5867ac47b07118b5fec
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2024-06-28 18:55:04 -0300

    config: starting project

---
+++ src/main/java/br/com/rafael/microserviceemail/MicroServiceEmailApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafael.microserviceemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServiceEmailApplication.class, args);
+    }
+
+}
